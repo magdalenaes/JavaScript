7e5a48be8405884cfee362a48e6dc8fcd3c70826
Magdalena Sremska <magdalena.sremska@gmail.com>
2020-02-07 03:06:41 +0100

    js, create new element on click

---
+++ Ex1/main.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("button");
+let number = 1;
+
+const addElement = () => {
+
+    const div = document.createElement("div");
+    div.textContent = number;
+
+    if(number % 5 == 0) {
+        div.classList.add("circle");
+    }
+
+
+    document.body.appendChild(div);
+    number++;   
+}
+
+btn.addEventListener("click", addElement);
