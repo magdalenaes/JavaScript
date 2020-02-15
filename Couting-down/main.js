61a78bf81849bb7dce640b0d27d20fa9fb3d902f
Magdalena Sremska <magdalena.sremska@gmail.com>
2020-02-15 17:42:01 +0100

    counting down, timer, setInterval

---
+++ Couting-down/main.js
@@ -0,0 +1,24 @@
+const endTime = new Date("2020-12-06 18:34:00").getTime();
+
+const spanD = document.querySelector("span.d");
+const spanH = document.querySelector("span.h");
+const spanM = document.querySelector("span.m");
+const spanS = document.querySelector("span.s");
+
+setInterval(() => {
+  const nowTime = new Date().getTime();
+  const time = endTime - nowTime;
+  const days = Math.floor(endTime / (1000 * 60 * 60 * 24) - nowTime / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+  let minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+  let seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  spanD.textContent = days;
+  spanH.textContent = hours;
+  spanM.textContent = minutes;
+  spanS.textContent = seconds;
+}, 1000);
